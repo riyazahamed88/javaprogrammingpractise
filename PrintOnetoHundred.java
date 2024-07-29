1449e15730e4aa759e894a4202569b69986f9e29
Riyaz Ahamed Sabibullah <163082517+riyazahamed88@users.noreply.github.com>
2024-07-29 11:18:01 -0400

    Add files via upload

---
+++ PrintOnetoHundred.java
@@ -0,0 +1,16 @@
+package javaproject;
+
+public class PrintOnetoHundred {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int one = 'A'/'A';
+		String s1 ="..........";
+		for( int i=one; i<=(s1.length() * s1.length()); i++) {
+			System.out.println(i);
+		}
+	}
+
+}
